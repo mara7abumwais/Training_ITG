aa9382acea6cbf3e2334777be0caefc538aaa053
mara7abumwais <1652001.com@gmail.com>
2023-11-05 00:00:12 +0200

    enhance js code

--- assets/js/createAccount.js
+++ assets/js/createAccount.js
@@ -3,6 +3,24 @@ const register_form = document.getElementById('register_form');
 
 register_form.addEventListener('submit', (event)=> {
 
+    const validateInput = (element, regex, errorElement, errorMessage, regexMsg) => {
+        const value = element.value.trim();
+        if (!value) {
+            errorElement.innerHTML = errorMessage;
+            element.classList.add('invalid');
+            return false;
+        } else {
+            errorElement.innerHTML = '';
+            element.classList.remove('invalid');
+            if (regex && !regex.test(value)) {
+                errorElement.innerHTML = `Please enter a valid ${regexMsg}`;
+                element.classList.add('invalid');
+                return false;
+            }
+        }
+        return true;
+    };
+
     const register_nameInput = document.getElementById('register_name');
     const register_emailInput = document.getElementById('register_email');
     const register_passwordInput = document.getElementById('register_password');
@@ -17,29 +35,15 @@ register_form.addEventListener('submit', (event)=> {
     const phoneRegex = /^[0-9]{10}$/;
     const passwordRegex = /^(?=.*[A-Za-z])[A-Za-z0-9]{8,}$/;
 
-    nameError.innerHTML = '';
-    emailError.innerHTML = '';
-    passwordError.innerHTML = '';
-    phoneError.innerHTML = '';
+    const isValid = [
+        validateInput(register_nameInput, null, nameError, 'Name is required', null),
+        validateInput(register_emailInput, emailRegex, emailError, 'Email address is required', 'Please enter a valid email address'),
+        validateInput(register_passwordInput, passwordRegex, passwordError, 'Passowrd is required',  'Password must be at least 8 characters, with at least one letter'),
+        validateInput(register_phoneInput, phoneRegex, phoneError, 'Phone is required', 'Please enter a valid 10-digit phone number'),
+    ].every(Boolean);
 
-    if (register_nameInput.value.trim() === '') {
-        nameError.textContent = 'Name is required';
-    }
-
-    if (!register_emailInput.value.match(emailRegex)) {
-        emailError.textContent = 'Please enter a valid email address';
-    }
-
-    if (!register_passwordInput.value.match(passwordRegex)) {
-        passwordError.textContent = 'Password must be at least 8 characters, with at least one letter.';
-    }
-
-    if (!register_phoneInput.value.match(phoneRegex)) {
-        phoneError.textContent = 'Please enter a valid 10-digit phone number';
-    }
-
-    if (!(nameError.innerHTML === "" && emailError.innerHTML === "" && passwordError.innerHTML === "" && phoneError.innerHTML === "")) {
+    if (!isValid) {
         event.preventDefault();
-    }
+    } 
 });
 
--- assets/js/order.js
+++ assets/js/order.js
@@ -1,66 +1,71 @@
 const order_form = document.getElementById('order_form');
 const food_modal = document.getElementById("food_modal");
 const close_foodModel = document.getElementById('close_foodModel');
+const food_options = document.getElementsByName('food_options');
+const special_notes = document.getElementById('special_notes');
 
 // to get user name
 const urlSearchParams = new URLSearchParams(window.location.search);
 
-order_form.addEventListener('submit',(event)=>{
-    event.preventDefault();
-    
-    const food_options = document.getElementsByName('food_options');
-    let selected_food = "";
+const foodPrices = {
+    fried_chicken: 10,
+    pizza: 20,
+    burger: 30,
+};
 
+function getSelectedFood ()
+{
     for(let option of food_options)
     {
         if(option.checked)
         {
-            selected_food = option;
-            break;
+            return option.value;
         }
     }
+    return null;
+}
+
+function calculatePrice(selectedFood)
+{
+    let price = foodPrices[selectedFood];
+    console.log(price);
+    if(special_notes.value !== "")
+    {
+        price = price + 5;
+    }
+    return price;
+}
+
+order_form.addEventListener('submit',(event)=>{
+    event.preventDefault();
     
-    const special_notes = document.getElementById('special_notes');
-   // alert(special_notes.value);
-    //alert(selected_food.value);
-    
-    //modal data
+    //Receipt modal data
      // Get the modal elements using getElementById
     const user_name = document.getElementById('user_name');
     const order_date = document.getElementById('order_date');
     const user_price = document.getElementById('user_price');
     const user_order = document.getElementById('user_order');
     const user_notes = document.getElementById('user_notes');
-    let price;
+
     const currentdate = new Date();
     let datetime = currentdate.getDay() + "/" + currentdate.getMonth() + "/" + currentdate.getFullYear();
 
 
-     // Set the innerHTML of the modal elements
-    user_name.innerHTML = urlSearchParams.get("register_name");
-    user_order.innerHTML = selected_food.value; 
-    if(selected_food.value === "fried_chicken")
-        price = 10;
-    else if(selected_food.value === "pizza")
-        price = 20;
-    else
-    price = 30;
-
-    if(special_notes.value !== "")
-    {
-        price = price + 5;
-        user_notes.innerHTML = "Your special notes: " + special_notes.value;
-    }
-
-    user_price.innerHTML = "$"+price;
+     // Set the innerHTML of the modal receipt elements
+    user_name.innerHTML = urlSearchParams.get('register_name');
+    user_order.innerHTML = getSelectedFood();
+    user_price.innerHTML = "$"+calculatePrice(user_order.innerHTML);
     order_date.innerHTML = datetime;
+    user_notes.innerHTML = "Your special notes: " + special_notes.value;
 
-
-    //display modal
+    //Reset the
+    order_form.reset();
+    //display receipt modal
     food_modal.style.display = "block";
 
 });
 
+//close receipt modal
 close_foodModel.onclick = function() {
     food_modal.style.display = "none";
 }
