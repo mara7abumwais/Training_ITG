b36cbf3039a55142e616e6eca22d1e7d36b72ddd
mara7abumwais <1652001.com@gmail.com>
2023-12-28 15:07:43 +0200

    test get customer

--- tests/integration/customer.test.js
+++ tests/integration/customer.test.js
@@ -1,6 +1,7 @@
 import {server} from '../../app.js';
 import request from 'supertest';
 import { customerModel } from '../../DB/models/customer.model.js';
+import { Types } from 'mongoose';
 
 describe('/api/cutomer',()=>{
     afterEach(async()=>{
@@ -23,5 +24,31 @@ describe('/api/cutomer',()=>{
         });
     });
 
-    
+    describe('GET /:id ',()=>{
+        it('should retuen customer if valid id is passed',async()=>{
+            const customer = new customerModel({name:'marah1',email:"marah1@gmail.com",phone:"0599999999",country:"palestine"});
+            await customer.save();
+
+            const res = await request(server).get(`/api/customer/${customer._id}`);
+            expect(res.status).toBe(200);
+            expect(res.body.success).toBeTruthy();
+            expect(res.body.customer).toHaveProperty('name',customer.name); 
+        });
+
+        it('should retuen 404 if invalid id is passed',async()=>{
+            const res = await request(server).get('/api/customer/1');
+            expect(res.status).toBe(404);
+            expect(res.body.success).not.toBeTruthy();
+        });
+
+        it('should retuen 404 if no customer with the passed id',async()=>{
+            const customer = new customerModel({name:'marah1',email:"marah1@gmail.com",phone:"0599999999",country:"palestine"});
+            await customer.save();
+            
+            const id = new Types.ObjectId();
+            const res = await request(server).get('/api/customer/'+id);
+            expect(res.status).toBe(404);
+            expect(res.body.success).not.toBeTruthy();
+        });
+    });
 });
